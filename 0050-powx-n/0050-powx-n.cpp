73fead121647382325eb43be84094fe666b2ba6e
Anurag Singh Thakur <120914452+Anurag29Nitrr@users.noreply.github.com>
2024-07-23 01:21:50 +0530

    Time: 16 ms (21.12%), Space: 37.7 MB (11.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+
+    double f( double x,int n){
+        if(n==0) return 1;
+         return x*f(x,n-1);
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n==INT_MAX) return (x==1) ? 1 : (x==-1) ?-1 :0;
+                if(n==INT_MIN) return (x==1 || x==-1) ? 1:0;
+
+                double num=1;
+                if(n>=0){
+                    num=f(x,n);
+                }
+                else{
+                    n=-n;
+                    num=f(x,n);
+                    num=1/num;
+                }
+                return num;
+
         
     }
 };
