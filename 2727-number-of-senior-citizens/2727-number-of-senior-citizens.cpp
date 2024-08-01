af7cbcfdffc12ffb2d183c93e6efa6d7cb98cb27
Anurag Singh Thakur <120914452+Anurag29Nitrr@users.noreply.github.com>
2024-08-01 06:20:45 +0530

    Time: 9 ms (23.77%), Space: 17.6 MB (16.71%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -3,17 +3,19 @@ public:
     int countSeniors(vector<string>& details) {
 
         int count=0;
-        int temp=0;
-        string s="";
+        // int temp=0;
+        // string s="";
         for( int i=0;i<details.size();i++){
+            int temp=0;
+        string s="";
             s+=details[i][11];
             s+=details[i][12];
             temp= stoi(s);
             if(temp>60){
                 count++;
             }
-            s="";
-            temp=0;
+            // s="";
+            // temp=0;
         }
         return count;
         
