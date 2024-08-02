1e1d129837d447459f45e069b6dc1416ae7fbd5d
Anurag Singh Thakur <120914452+Anurag29Nitrr@users.noreply.github.com>
2024-08-02 18:55:22 +0530

    Time: 69 ms (53.24%), Space: 83 MB (57.56%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int total=accumulate(nums.begin(),nums.end(),0);
+        int total=accumulate(nums.begin(),nums.end(),0);//TOTAL 1'S
     int n=nums.size();
 
-     if(total == 0 || total ==n) return 0;
+     if(total == 0 || total ==n) return 0;// BASE CASES TRIVIAL
     // int n=nums.size();
 int curr=0;
     //int maxi=0;
@@ -12,13 +12,14 @@ int curr=0;
         curr+=nums[i];
 
     }
-    int maxi=curr;
+    int maxi=curr;//MAXI IS NO OF 1/S IN FIRST WONDOW
+    //CURR IS TO BE UPATED 
 
 
      for(int i=0;i<n;i++){
         curr-=nums[i];
         curr+=nums[(i+total)%n];
-        maxi=max(maxi,curr);
+        maxi=max(maxi,curr);//MAX OF NO OF 1 IN CURR VS IN FIRST ONE
      }
 
      return total-maxi;
